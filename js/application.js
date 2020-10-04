744ac74661b1809b7c3a92571b5e402ded8bbb76
Eduardo Fernandez <edufernandez4@gmail.com>
2020-10-04 23:13:18 +0200

    feat: añadida vista de resultados de busqueda. Se genera token a partir de la api registrada y se guarda en active user en local storage para ser usada en las queries a la bd

--- js/application.js
+++ js/application.js
@@ -16,6 +16,22 @@ function main(){
         loginButton.addEventListener('click', onClickLogin)
     }
 
+    const searchButton =  document.querySelector('#query_button')    
+    if(searchButton){
+        searchButton.addEventListener('click', onClickSearch)
+        const active_user = window.localStorage.getItem(activeUserLocalStorage) ?
+        JSON.parse(window.localStorage.getItem(activeUserLocalStorage)) : undefined
+        let token = ""
+        let user = ""
+        if(active_user){
+            token = active_user.token
+            user = active_user.usuario
+            document.querySelector('#display_user').innerHTML = `<b>${user}</b>`
+            document.querySelector('#display_token').innerHTML = `<b>${token}</b>`
+        }
+         
+    }
+
     function onClickSingUp() {
         const singupForm = document.querySelector('#singup_form')
         const inputs = [...singupForm.querySelectorAll('input')]
@@ -58,8 +74,7 @@ function main(){
         console.log(window.localStorage)    
     }
 
-    function onClickLogin() {
-        console.log("usesss")
+    function onClickLogin() {        
         const error_box =  document.querySelector('#error_box')
         const error_text_elem =  document.querySelector('#error_text')
         const loginForm = document.querySelector('#login_form')
@@ -67,16 +82,10 @@ function main(){
         JSON.parse(window.localStorage.getItem(usersLocalStorage)) : []
         let username = loginForm.querySelector('input[name="username"]').value;        
         let password = loginForm.querySelector('input[name="password"]').value;        
-        for(let user of users){
-            console.log(user)
+        for(let user of users){            
             if(username === user.usuario){
                 if(password === user.password){
-                    window.localStorage.setItem(activeUserLocalStorage, JSON.stringify(user))
-                    console.log(window.localStorage)
-                    error_text_elem.innerHTML = `Login correcto. Usuario activo: <b>${username}</b> -> <a href="films.html"> Consultar Peliculas </a>`
-                    error_box.classList.remove("hidden")  
-                    error_box.classList.remove("error")
-                    error_box.classList.add("ok")
+                    user.token = generateToken(user)
                 }else{
                     error_text_elem.innerHTML = "Contraseña incorrecta"
                     error_box.classList.remove("hidden")
@@ -91,7 +100,53 @@ function main(){
                 error_box.classList.add("error")
                 //No se encuentra el usuario
             }
-        }
+        }        
+    }
+
+    function onClickSearch() {
+        console.log(window.localStorage)
+    }
+
+    function generateToken(user){
+        const method = 'GET'
+        let token = ""
+        const url = `https://api.themoviedb.org/3/authentication/token/new?api_key=${user.apikey}`   
+        // https://restcountries.eu/rest/v2/name/{}
+        /* const http = new XMLHttpRequest()
+        console.log(http)
+        http.onreadystatechange = ajaxCallback
+        http.addEventListener('readystatechange', ajaxCallback)
+        http.open(method, url)
+        http.send(null) */
+
+        // fetch
+        // axios
+
+        fetch(url)
+        .then( resp => {
+            console.log(resp)
+            if (resp.status < 200 || resp.status >= 300) {
+                console.log(resp.statusText)
+                throw new Error('HTTP Error ' + resp.status)
+            }
+            return resp.json()
+        })
+        .then( data =>  processToken(data, user))
+        .catch (error => alert(error.message))        
+        
+    }
+
+    function processToken(data, user){
+        const error_box =  document.querySelector('#error_box')
+        const error_text_elem =  document.querySelector('#error_text')
+        user.token = data.request_token
+        window.localStorage.setItem(activeUserLocalStorage, JSON.stringify(user))
+        console.log(window.localStorage)
+        console.log(data)
+        error_text_elem.innerHTML = `Login correcto.<br><br>Token Created: ${data.success}<br>Token: ${data.request_token}<br>Expires at: ${data.expires_at}<br><br>Usuario activo: <b>${user.usuario}</b> -> <a href="films.html"> Consultar Peliculas </a>`
+        error_box.classList.remove("hidden")  
+        error_box.classList.remove("error")
+        error_box.classList.add("ok")
     }
 
     
